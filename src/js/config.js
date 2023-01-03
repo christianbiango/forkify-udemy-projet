d5f45c7295f1556228a3c4491495de055ec75a78
Melix1 <biangojunior@gmail.com>
2023-01-03 22:13:25 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+// Toutes les variables constantes qui seront réutilisées dans le projet. Celles qui définissent des data importantes pour l'app, pas toutes les var.
+// La const changera pas donc c'est un common practice de mettre des majuscules pour la nommer.
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ac18880a-49d9-4d69-acfd-0205f51204a4';
+export const MODAL_CLOSE_SEC = 2.5;
