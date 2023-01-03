819ffa7405c05bf4c0d446f4d85ad91a2cb2869d
Melix1 <biangojunior@gmail.com>
2023-01-03 22:54:34 +0100

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -10,7 +10,6 @@ import addRecipeView from './views/addRecipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
-console.log('hey');
 if (module.hot) {
   module.hot.accept();
 }
